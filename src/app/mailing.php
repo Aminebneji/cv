4ae0e6b0490f575c417b24f8449be77952965f79
Aminebneji <aminebneji@gmail.com>
2022-11-08 17:26:03 +0100

    rectification (before Xampp tests)

--- src/app/mailing.php
+++ src/app/mailing.php
@@ -1,31 +1,31 @@
 <?php
+echo 'Current PHP version: '. Phpversion();
+$to = "aminebneji@gmail.com";
+$subject = "Tu as reçu un message depuis ton site !";
 
-$to = "aminebneji@gmail.com"
-$subject = "Tu as reçu un message depuis ton site !"
-
-$nom = addslashes($_POST['Nom']) ; 
+$nom = addslashes($_POST['Nom']);
 $prenom = addslashes($_POST['Prenom']);
-$mail = addslashes($_POST['Mail']); 
-$phone = addslashes($_POST['Phone'])
-$msg = addslashes($_POST['messageContent']); 
+$mail = addslashes($_POST['Mail']);
+$phone = addslashes($_POST['Phone']);
+$msg = addslashes($_POST['messageContent']);
 
 
-$message = "tu as reçus un message de la part de :".$prenom.".\n" ; 
-$message. ="tu peux le contacter au :".$Phone.".\n\n";
+$message = "tu as reçus un message de la part de :" . $prenom . ".\n";
+$message . "tu peux le contacter au :" . $Phone . ".\n\n";
 
 
-$header = 'From: '.$mail . "\r\n" . 
-          'Reply-To:'.$mail . "\r\n" . 
-          'X-Mailer: PHP/' . phpversion(); 
-          
- if(mail($to , $subject , $message , $header)){ 
-    echo json_encode([ 
-        'status' => 100, 
+$header = 'From: ' . $mail . "\r\n" .
+    'Reply-To:' . $mail . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if (mail($to, $subject, $message, $header)) {
+    echo json_encode([
+        'status' => 100,
         'message' => 'Envoi confirmé'
     ]);
- }else{ 
-    echo json_encode([ 
-        'status' => 101, 
+} else {
+    echo json_encode([
+        'status' => 101,
         'message' => 'Echec de l`envoi '
-    ])
- }         
+    ]);
+}
